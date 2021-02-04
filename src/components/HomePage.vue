6f4fbc6d820ff879c2cb1e58465ca257dc0e34e4
Michail Koptsis <mieciel-1908@hotmail.com>
2021-02-04 15:05:13 +0200

    add section glass to main and style it properly

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -13,8 +13,23 @@ export default {};
 </script>
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background: linear-gradient(to right top, #65dfc9, #6cdbeb);
+}
 main {
-  min-height: 150vh;
+  min-height: 100vh;
   background: linear-gradient(to right top, #65dfc9, #6cdbeb);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.glass{
+    background: white;
+    min-height: 80vh;
+    width: 60%;
 }
 </style>
