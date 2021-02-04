06eada12ffc263227e09102f1ad31112aa6e74f3
Michail Koptsis <mieciel-1908@hotmail.com>
2021-02-04 14:58:52 +0200

    add main in html and style it

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,14 +1,20 @@
 <template>
+  <main>
     <h1>MICHAIL KOPTSIS</h1>
     <h3>Software Engineer</h3>
+    <section class="glass"></section>
+  </main>
+  <div class="circle1"></div>
+  <div class="circle2"></div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+main {
+  min-height: 150vh;
+  background: linear-gradient(to right top, #65dfc9, #6cdbeb);
+}
 </style>
